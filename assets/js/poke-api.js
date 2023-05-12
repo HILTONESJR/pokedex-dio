b6961dc36b46bcc052f23ad11dd90ba0bf2bce1a
HILTONESJR <bmxsts@gmail.com>
2023-05-12 18:03:49 -0300

    comentarios

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -10,8 +10,8 @@ pokeApi.getPokemons = (offset = 0, limit = 5) => {
     const url = `http://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
     return fetch(url)
-        .then((response) => response.json())
-        .then((jsonBody) => jsonBody.results)
+        .then((response) => response.json()) // converte reponse para json
+        .then((jsonBody) => jsonBody.results) // converte json para results
         .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
         .then((detailRequests) => Promise.all(detailRequests))
         .then((pokemonsDetails) => pokemonsDetails)

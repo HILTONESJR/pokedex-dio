5366251b99a4862d138b22919965e5d4da56be57
HILTONESJR <bmxsts@gmail.com>
2023-05-13 13:07:06 -0300

    convertendo o response e transformando em detalhes adicionando Type, types, photo.

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,22 +2,22 @@
 const offset = 0;
 const limit = 10;
 const url = `http://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+  
 
 function convertePokemonToLi(pokemon) {
     return `
-         <li class=" pokemon">
-    <span class="number">#001</span>
-    <span class="name">${pokemon.name}</span>
-    <div class="detail">
+         <li class="pokemon ${pokemon.type}">
+        <span class="number">#${pokemon.number}</span>
+        <span class="name">${pokemon.name}</span>
+        <div class="detail">
         <ol class="types">
-            <li class="type">Glass</li>
-            <li class="type">Poison</li>
-        </ol>    
-    <img src="https://media.tenor.com/6nHb-yHC2pkAAAAi/bulbasaur.gif" alt="${pokemon.name}">
+            ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}          
+        </ol>
+
+    <img src="${pokemon.photo}" alt="${pokemon.name}">
     </div>
 </li>
-`
-}
+`}
 
 const pokemonList = document.getElementById('pokemonList')
 

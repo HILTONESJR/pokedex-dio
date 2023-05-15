16535f0b9f32635d9ab03f8c0bec38aab376b35e
HILTONESJR <bmxsts@gmail.com>
2023-05-15 10:42:42 -0300

    adicionando função click para abrir proxima lista

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,26 +1,34 @@
 
-const offset = 0;
-const limit = 10;
-const url = `http://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
-  
-
-function convertePokemonToLi(pokemon) {
-    return `
-         <li class="pokemon ${pokemon.type}">
-        <span class="number">#${pokemon.number}</span>
-        <span class="name">${pokemon.name}</span>
-        <div class="detail">
-        <ol class="types">
-            ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}          
-        </ol>
-
-    <img src="${pokemon.photo}" alt="${pokemon.name}">
-    </div>
-</li>
-`}
 
 const pokemonList = document.getElementById('pokemonList')
+const loadMoreButton = document.getElementById('loadMoreButton')
+const limit = 5;
+let offset = 0;
 
-pokeApi.getPokemons().then((pokemons = []) => {
-    pokemonList.innerHTML += pokemons.map(convertePokemonToLi).join('')
+
+function loadPokemonItens(offset, limit) {
+    pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+
+        const newHtml = pokemons.map((pokemon) => `
+            <li class="pokemon ${pokemon.type}">
+                <span class="number">#${pokemon.number}</span>
+                <span class="name">${pokemon.name}</span>
+                <div class="detail">
+                <ol class="types">
+                    ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}          
+                </ol>
+
+                <img src="${pokemon.photo}" alt="${pokemon.name}">
+                </div>
+                </li>
+            `).join('')
+        pokemonList.innerHTML += newHtml
+    })
+}
+
+loadPokemonItens(offset, limit)
+
+loadMoreButton.addEventListener('click', () => {
+    offset += limit
+    loadPokemonItens(offset, limit)
 })
